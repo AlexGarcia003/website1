c2c27e75b4c8665ef5c3a1deeecd2b4e06d39dd9
ReggieGilliam <142766644+ReggieGilliam@users.noreply.github.com>
2024-03-11 22:05:45 +0000

    Mon, Mar 11, 2024, 3:05 PM -07:00

--- js2/LandingPage.js
+++ js2/LandingPage.js
@@ -3,15 +3,24 @@ open = document.getElementtListenerById('open')
 modal = document.getElementById('modal')
 close = document.getElementById('close')
 
-
+//togal nav
 toggle.addEventListener('click', () => {
     document.body.classList.toggle('show-nav')
 })
 
+//show modal
 open.addEventListener('click', () => {
     modal.classList.add('show-modal')
 })
 
+//hide modal
 close.addEventListener('click', () => {
-    
+    modal.classList.remove('show-model')
+})
+
+//hide modal on outside click
+window.addEventListener('click', (e) => {
+    if (e.target == modal) {
+        modal.classList.remove('show')
+    }
 })
